2627b69c64480dbea88893c3d0b1a6d8dd874c31
Mahiro Ando <ma514y@gmail.com>
2012-10-24 08:47:39 +0900

    refactoring JavaScript adapter

--- lib/EJS/Template/JSEngine/JavaScript.pm
+++ lib/EJS/Template/JSEngine/JavaScript.pm
@@ -5,8 +5,9 @@ use warnings;
 package EJS::Template::JSEngine::JavaScript;
 use base 'EJS::Template::JSEngine';
 
-use Scalar::Util qw(reftype);
+use Encode;
 use JavaScript;
+use Scalar::Util qw(reftype);
 
 =head2 new
 
@@ -27,69 +28,55 @@ sub bind {
 	my ($self, $variables) = @_;
 	my $context = $self->context;
 	
+	my $assign_value;
 	my $assign_hash;
 	my $assign_array;
 	
+	$assign_value = sub {
+		my ($parent_path, $name, $source_ref) = @_;
+		
+		my $reftype = reftype $$source_ref;
+		my $path = $parent_path ? "$parent_path.$name" : $name;
+		
+		if ($reftype) {
+			if ($reftype eq 'HASH') {
+				#$context->bind_value($path, {});
+				JavaScript::Context::jsc_bind_value($context, $parent_path, $name, {});
+				$assign_hash->($path, $$source_ref);
+			} elsif ($reftype eq 'ARRAY') {
+				#$context->bind_value($path, []);
+				JavaScript::Context::jsc_bind_value($context, $parent_path, $name, []);
+				$assign_array->($path, $$source_ref);
+			} elsif ($reftype eq 'CODE') {
+				#$context->bind_function($path, $$source_ref);
+				JavaScript::Context::jsc_bind_value($context, $parent_path, $name, $$source_ref);
+			} else {
+				# ignore?
+			}
+		} else {
+			#$context->bind_value($path, $$source_ref);
+			JavaScript::Context::jsc_bind_value($context, $parent_path, $name, $$source_ref);
+		}
+	};
+	
 	$assign_hash = sub {
-		my ($source, $parent_path) = @_;
+		my ($parent_path, $source) = @_;
 		
 		for my $name (keys %$source) {
-			my $ref = reftype $source->{$name};
-			my $path = $parent_path ? "$parent_path.$name" : $name;
-			
-			if ($ref) {
-				if ($ref eq 'HASH') {
-					#$context->bind_value($path, {});
-					JavaScript::Context::jsc_bind_value($context, $parent_path, $name, {});
-					$assign_hash->($source->{$name}, $path);
-				} elsif ($ref eq 'ARRAY') {
-					#$context->bind_value($path, []);
-					JavaScript::Context::jsc_bind_value($context, $parent_path, $name, []);
-					$assign_array->($source->{$name}, $path);
-				} elsif ($ref eq 'CODE') {
-					#$context->bind_function($path, $source->{$name});
-					JavaScript::Context::jsc_bind_value($context, $parent_path, $name, $source->{$name});
-				} else {
-					# ignore?
-				}
-			} else {
-				#$context->bind_value($path, $source->{$name});
-				JavaScript::Context::jsc_bind_value($context, $parent_path, $name, $source->{$name});
-			}
+			$assign_value->($parent_path, $name, \$source->{$name});
 		}
 	};
 	
 	$assign_array = sub {
-		my ($source, $parent_path) = @_;
+		my ($parent_path, $source) = @_;
 		my $len = scalar(@$source);
 		
 		for (my $i = 0; $i < $len; $i++) {
-			my $ref = reftype $source->[$i];
-			my $path = "$parent_path\[$i]";
-			
-			if ($ref) {
-				if ($ref eq 'HASH') {
-					#$context->bind_value($path, {});
-					JavaScript::Context::jsc_bind_value($context, $parent_path, $i, {});
-					$assign_hash->($source->[$i], $path);
-				} elsif ($ref eq 'ARRAY') {
-					#$context->bind_value($path, []);
-					JavaScript::Context::jsc_bind_value($context, $parent_path, $i, []);
-					$assign_array->($source->[$i], $path);
-				} elsif ($ref eq 'CODE') {
-					#$context->bind_function($path, $source->[$i]);
-					JavaScript::Context::jsc_bind_value($context, $parent_path, $i, $source->[$i]);
-				} else {
-					# ignore?
-				}
-			} else {
-				#$context->bind_value($path, $source->[$i]);
-				JavaScript::Context::jsc_bind_value($context, $parent_path, $i, $source->[$i]);
-			}
+			$assign_value->($parent_path, $i, \$source->[$i]);
 		}
 	};
 	
-	$assign_hash->($variables, '');
+	$assign_hash->('', $variables);
 	return $context;
 }
 
