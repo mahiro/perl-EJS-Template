87a97878f97042c88a89f39c0ab8d2f004f5a6e1
Mahiro Ando <ma514y@gmail.com>
2012-10-26 01:34:55 +0900

    refactoring and taint check for v8

--- lib/EJS/Template/JSEngine/JavaScript/V8.pm
+++ lib/EJS/Template/JSEngine/JavaScript/V8.pm
@@ -6,6 +6,7 @@ package EJS::Template::JSEngine::JavaScript::V8;
 use base 'EJS::Template::JSEngine';
 
 use JavaScript::V8;
+use Scalar::Util qw(reftype tainted);
 
 =head2 new
 
@@ -25,8 +26,52 @@ sub bind {
 	my ($self, $variables) = @_;
 	my $context = $self->context;
 	
-	for my $name (keys %$variables) {
-		$context->bind($name, $variables->{$name});
+	my $assign_value;
+	my $assign_hash;
+	my $assign_array;
+	
+	$assign_value = sub {
+		my ($target_ref, $source_ref) = @_;
+		my $reftype = reftype $$source_ref;
+		
+		if ($reftype) {
+			if ($reftype eq 'HASH') {
+				$assign_hash->($$target_ref = {}, $$source_ref);
+			} elsif ($reftype eq 'ARRAY') {
+				$assign_array->($$target_ref = [], $$source_ref);
+			} elsif ($reftype eq 'CODE') {
+				$$target_ref = $$source_ref;
+			} else {
+				# ignore?
+			}
+		} else {
+			$$target_ref = tainted($$source_ref) ?
+					do {$$source_ref =~ /(.*)/s; $1} : $$source_ref;
+		}
+	};
+	
+	$assign_hash = sub {
+		my ($target, $source) = @_;
+		
+		for my $name (keys %$source) {
+			$assign_value->(\$target->{$name}, \$source->{$name});
+		}
+	};
+	
+	$assign_array = sub {
+		my ($target, $source) = @_;
+		my $len = scalar(@$source);
+		
+		for (my $i = 0; $i < $len; $i++) {
+			$assign_value->(\$target->[$i], \$source->[$i]);
+		}
+	};
+	
+	my $clone = {};
+	$assign_hash->($clone, $variables);
+	
+	for my $name (keys %$clone) {
+		$context->bind($name, $clone->{$name});
 	}
 	
 	return $context;
