3a09633f82733068957466b860a4f928d74c30cc
Mahiro Ando <ma514y@gmail.com>
2012-10-28 18:16:29 +0900

    increment version to 0.03

--- lib/EJS/Template.pm
+++ lib/EJS/Template.pm
@@ -13,11 +13,11 @@ EJS::Template - EJS (Embedded JavaScript) template engine
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
