3d8c177226e84fc04b21460a98981332ad0b737e
Mahiro Ando <ma514y@gmail.com>
2017-02-07 00:52:09 +0900

    update version info

--- lib/EJS/Template.pm
+++ lib/EJS/Template.pm
@@ -15,11 +15,11 @@ use EJS::Template::Parser;
 
 =head1 VERSION
 
-Version 0.07
+Version 0.08
 
 =cut
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 our @CONFIG_KEYS = qw(engine escape);
 our $context;
